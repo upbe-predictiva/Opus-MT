c1be829df0512373215cb272057c25726480b6c8
Ana Gomez Andrades <ana@predictiva.io>
2021-09-15 12:48:47 +0200

    Update download_models.sh

--- download_models.sh
+++ download_models.sh
@@ -1,4 +1,4 @@
-curl -o models/es-ca.zip https://object.pouta.csc.fi/OPUS-MT-models/es-ca/opus-2020-01-16.zip
+curl -o models/es-ca.zip https://object.pouta.csc.fi/Tatoeba-MT-models/spa-cat/opus-2021-02-19.zip
 curl -o models/en-es.zip https://object.pouta.csc.fi/OPUS-MT-models/en-es/opus-2019-12-04.zip
 unzip models/es-ca.zip -d models/es-ca
 unzip models/en-es.zip -d models/en-es
